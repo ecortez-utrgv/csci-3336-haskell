e9001a50a26ac1a59cf8a2ba96a5c7cffe786a56
ecortez-utrgv <eduardo.cortez04@utrgv.edu>
2025-07-09 04:27:49 +0000

    rework currying for clarity

--- part2/05-currying.hs
+++ part2/05-currying.hs
@@ -5,6 +5,11 @@ curryableFunction :: Int -> Int -> Int  -- Int -> (Int -> Int)
 curryableFunction a b = a + b
 
 curriedFunction :: Int -> Int
-curriedFunction {-b-} = curryableFunction 5 {-b-} -- eta reduction
+curriedFunction b = curryableFunction 5 b
+{-
+by eta reduction, could be written as
+curriedFunction = curryableFunction 5
+-}
 
-seven :: Int = curriedFunction 2
+seven :: Int
+seven = curriedFunction 2
